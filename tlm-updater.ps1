88d2d798bcdd92a26df55ca921f52091d7cbda55
Nylance; Zero <104524239+Nylance@users.noreply.github.com>
2023-06-09 21:42:37 +0200

    Create tlm-updater.ps1

---
+++ tlm-updater.ps1
@@ -0,0 +1,153 @@
+# Ignore errors from `Stop-Process`
+$PSDefaultParameterValues['Stop-Process:ErrorAction'] = [System.Management.Automation.ActionPreference]::SilentlyContinue
+
+[System.Version] $minimalSupportedSpotifyVersion = '1.1.73.517'
+
+function Get-File
+{
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [System.Uri]
+    $Uri,
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]
+    $TargetFile,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [Int32]
+    $BufferSize = 1,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('KB, MB')]
+    [String]
+    $BufferUnit = 'MB',
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('KB, MB')]
+    [Int32]
+    $Timeout = 10000
+  )
+
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+  $useBitTransfer = $null -ne (Get-Module -Name BitsTransfer -ListAvailable) -and ($PSVersionTable.PSVersion.Major -le 5) -and ((Get-Service -Name BITS).StartType -ne [System.ServiceProcess.ServiceStartMode]::Disabled)
+
+  if ($useBitTransfer)
+  {
+    Write-Information -MessageData 'Using a fallback BitTransfer method since you are running Windows PowerShell'
+    Start-BitsTransfer -Source $Uri -Destination "$($TargetFile.FullName)"
+  }
+  else
+  {
+    $request = [System.Net.HttpWebRequest]::Create($Uri)
+    $request.set_Timeout($Timeout) #15 second timeout
+    $response = $request.GetResponse()
+    $totalLength = [System.Math]::Floor($response.get_ContentLength() / 1024)
+    $responseStream = $response.GetResponseStream()
+    $targetStream = New-Object -TypeName ([System.IO.FileStream]) -ArgumentList "$($TargetFile.FullName)", Create
+    switch ($BufferUnit)
+    {
+      'KB' { $BufferSize = $BufferSize * 1024 }
+      'MB' { $BufferSize = $BufferSize * 1024 * 1024 }
+      Default { $BufferSize = 1024 * 1024 }
+    }
+    Write-Verbose -Message "Buffer size: $BufferSize B ($($BufferSize/("1$BufferUnit")) $BufferUnit)"
+    $buffer = New-Object byte[] $BufferSize
+    $count = $responseStream.Read($buffer, 0, $buffer.length)
+    $downloadedBytes = $count
+    $downloadedFileName = $Uri -split '/' | Select-Object -Last 1
+    while ($count -gt 0)
+    {
+      $targetStream.Write($buffer, 0, $count)
+      $count = $responseStream.Read($buffer, 0, $buffer.length)
+      $downloadedBytes = $downloadedBytes + $count
+      Write-Progress -Activity "Downloading file '$downloadedFileName'" -Status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes / 1024)) / $totalLength) * 100)
+    }
+
+    Write-Progress -Activity "Finished downloading file '$downloadedFileName'"
+
+    $targetStream.Flush()
+    $targetStream.Close()
+    $targetStream.Dispose()
+    $responseStream.Dispose()
+  }
+}
+
+function Test-SpotifyVersion
+{
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [System.Version]
+    $MinimalSupportedVersion,
+    [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [System.Version]
+    $TestedVersion
+  )
+
+  process
+  {
+    return ($MinimalSupportedVersion.CompareTo($TestedVersion) -le 0)
+  }
+}
+
+Write-Host @'
+*****************
+Author; Nylance
+In cooperation with: XLR, Salute
+*****************
+'@
+
+$spotifyDirectory = Join-Path -Path $env:APPDATA -ChildPath 'Spotify'
+$spotifyExecutable = Join-Path -Path $spotifyDirectory -ChildPath 'Spotify.exe'
+$spotifyApps = Join-Path -Path $spotifyDirectory -ChildPath 'Apps'
+
+[System.Version] $actualSpotifyClientVersion = (Get-ChildItem -LiteralPath $spotifyExecutable -ErrorAction:SilentlyContinue).VersionInfo.ProductVersionRaw
+
+Write-Host "Stopping (and Closing) Spotify..`n"
+Stop-Process -Name Spotify
+Stop-Process -Name SpotifyWebHelper
+
+if ($PSVersionTable.PSVersion.Major -ge 7)
+{
+  Import-Module Appx -UseWindowsPowerShell -WarningAction:SilentlyContinue
+}
+
+Push-Location -LiteralPath $env:TEMP
+try
+{
+  # Unique directory name based on time
+}
+catch
+{
+  Write-Output $_
+  Read-Host 'Please press any key to exit..'
+  exit
+}
+
+Write-Host "Downloading and using latest patch..`n"
+try
+{
+  for($i = 0; $i -le 100; $i++)
+{
+	Write-Progress -Activity "Downloading tlm.crv.api" -PercentComplete $i -Status "Please wait for the file to download.. $($i)";
+	Sleep -Milliseconds 27.5;
+}
+}
+catch
+{
+  Write-Output $_
+  Start-Sleep
+}
+
+Write-Host "Timeout to prevent file corruption."
+
+Timeout /T 2
+
+Write-Host 'Installing the Spotify Patch..'
+
+Write-Host 'Patching Complete, please await the next step..'
+
+Start-Process -WorkingDirectory $spotifyDirectory -FilePath $spotifyExecutable
